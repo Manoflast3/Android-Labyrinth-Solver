8f0a11769789d05f14599f3d95ee0ed2bd160ff5
Derek <zengzhuoxi@gmail.com>
2016-07-10 22:53:56 -0400

    The wall follower logic for the up case has been done.

--- app/src/main/java/ca/uwaterloo/lab3_201_03/PathFinder.java
+++ app/src/main/java/ca/uwaterloo/lab3_201_03/PathFinder.java
@@ -28,6 +28,8 @@ public class PathFinder {
     private PointF temppoint = lastpoint;
     private final float stepValue = 0.5f;
 
+    private LineSegment followedWall;
+
     // I also need the angle of the map. This angle is measured from the vertical, from 0 to 180 degrees.
     // (doesn't go to 180 though).
     private final double angle = 0.0f;
@@ -39,23 +41,92 @@ public class PathFinder {
 
         // If a direct path can be made.
         if(source.calculateIntersections(userStart, userEnd).isEmpty()){
-            userPath.add(userEnd);
+
         }
         else{
-            setupPath(userPath, source);
+            setupPath(source);
+            userPath.add(lastpoint);
+
+            // Proceed with Wall following algorithm IF the solution has not yet been found.
+            if (!source.calculateIntersections(lastpoint,userEnd).isEmpty()){
+                while (!source.calculateIntersections(lastpoint, userEnd).isEmpty()) {
+                    switch (cDirection) {
+                        case "up":
+                            takeStep(temppoint, cDirection);
+                            // if the next step goes through a wall.
+                            if (!source.calculateIntersections(lastpoint, temppoint).isEmpty()){
+                                lastpoint.set(source.calculateIntersections(lastpoint, temppoint).get(0).getPoint());
+                                followedWall = source.getGeometryAtPoint(lastpoint).get(0);
+                                cDirection = "right";
+                            }
+                            // if the next step goes past the currently followed wall.
+                            else if (temppoint.y > Math.max(followedWall.start.y, followedWall.end.y)){
+                                lastpoint.set(Math.min(followedWall.start.x, followedWall.end.x), Math.max(followedWall.start.y, followedWall.end.y));
+                                followedWall = source.getGeometryAtPoint(lastpoint).get(0);
+                                cDirection = "left";
+                            }
+
+                            // take another step. 
+                            else {
+                                takeStep(temppoint, cDirection);
+                            }
+                            break;
+
+                        case "down":
+
+                            break;
+
+                        case "right":
+
+                            break;
+
+                        case "left":
+
+                            break;
+
+                        default:
+                    }
+                }
+            }
         }
+
+        userPath.add(userEnd);
 
         return userPath;
     }
 
-    public void setupPath(List<PointF> userPath, NavigationalMap source){
+    public void setupPath( NavigationalMap source){
         while(source.calculateIntersections(lastpoint, temppoint).isEmpty()){
-            takeStep(temppoint);
+            takeStep(temppoint, cDirection);
+            if (source.calculateIntersections(temppoint, userEnd).isEmpty()){
+                lastpoint.set(temppoint);
+                break;
+            }
         }
+        if (lastpoint!=temppoint) {
+            lastpoint.set(source.calculateIntersections(lastpoint, temppoint).get(0).getPoint());
+        }
+
+        followedWall = source.getGeometryAtPoint(lastpoint).get(0);
+        cDirection = "right";
+        sumofTurns++;
     }
 
-    public PointF takeStep(PointF temppoint){
-        temppoint.offset((float)(Math.sin(angle)*stepValue), (float)(Math.cos(angle)*stepValue));
+    public PointF takeStep(PointF temppoint, String cDirection){
+        switch (cDirection){
+            case "up":
+                temppoint.offset((float)(-Math.sin(angle)*stepValue), (float)(Math.cos(angle)*stepValue));
+                break;
+            case "down":
+                temppoint.offset((float)(Math.sin(angle)*stepValue), (float)(-Math.cos(angle)*stepValue));
+                break;
+            case "right":
+                temppoint.offset((float)(Math.cos(angle)*stepValue), (float)(Math.sin(angle)*stepValue));
+                break;
+            case "left":
+                temppoint.offset((float)(-Math.cos(angle)*stepValue), (float)(-Math.sin(angle)*stepValue));
+                break;
+        }
         return temppoint;
     }
 
